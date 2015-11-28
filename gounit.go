1d1d34ca29aad1469fb31ddbe0aa0af8fd7f8ce8
tolgabulca <tolgabulca@live.com>
2015-11-28 23:50:51 +0200

    First file initalized

--- gounit.go
+++ gounit.go
@@ -1,15 +1,6 @@
 package gounit
-import (
-	"fmt"
-)
 
-func main(){
-	if isEqual(1,1) {
-		fmt.Println("Oldu");
-	}
-}
-
-func isEqual(v1,v2 int) (bool){
+func isEqual(v1,v2 int) bool{
 	if v1 == v2 {
 		return true
 	} else {
